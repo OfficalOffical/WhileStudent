2fa9663fc5a6957235a1acbb55babe7b0d85c7a1
Sefa <sefakalkan_ts@hotmail.com>
2019-11-16 00:29:21 +0300

    Update Main.java

--- Java/OOP/phoneSimulator/Main.java
+++ Java/OOP/phoneSimulator/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	People people1 = new People("Ahmet",1);
-	People people2 = new People("Temha",2);
+	People people1 = new People("Jhon",1);
+	People people2 = new People("Gevrek",2);
 
 	people1.add(2);
-    people1.call(2);
+        people1.call(2);
 
 
 
