d9bc7c692da6a284310b7aa1fab7d969528cece6
Sefa <mail@sefakalkan.com>
2020-02-10 18:24:37 +0300

    Tetris game

---
+++ Java/OOP/Tetris/com/MCK/Main.java
@@ -0,0 +1,8 @@
+package com.MCK;
+
+public class Main {
+
+    public static void main(String[] args) {
+	new Window ();
+    }
+}
---
+++ Java/OOP/Tetris/com/MCK/Window.java
@@ -0,0 +1,35 @@
+package com.MCK;
+
+import javax.swing.JFrame;
+
+public class Window {
+
+    public static final int WIDTH = 316, HEIGHT = 638;
+    private JFrame window;
+    private Board board;
+
+    public Window(){
+        window = new JFrame("Tetris Game");
+        window.setSize(WIDTH, HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+
+        board = new Board();
+
+
+
+        window.add(board);
+        window.addKeyListener(board);
+
+
+        window.setVisible(true);
+    }
+
+
+
+    public static void main(String[] args) {
+        new Window();
+    }
+
+}
