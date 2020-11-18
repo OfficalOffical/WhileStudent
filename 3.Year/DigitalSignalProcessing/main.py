49e0896b544fd011e11fe364eeeda758423ea272
Sefa <mail@sefakalkan.com>
2020-11-18 13:51:38 +0300

    With HomeWorks

--- 3.Year/DigitalSignalProcessing/main.py
+++ 3.Year/DigitalSignalProcessing/main.py
@@ -1,23 +1,29 @@
 import numpy as np #numpy 1.19.4 does'nt work with pycharm 3.9 so i'm using 1.19.3
 import matplotlib.pyplot as plt
 
-#Unit step function and Ramp function
+def rectFunc(t):
+    """Rectangular Function"""
+    return  4 * (abs(t-2) <=2)
 
-def unitstep_func(t):
+def triangFunc(t):
+    """Triangular Function"""
+    return (1 - abs(t)) * (abs(t) < 1)
+
+def unitstepFunc(t):
     """Unit step function"""
-    return  1 * (t >= 0)
-def ramp_func(t):
+    return  5 * (t >= 5)
+def rampFunc(t):
     """Ramp function"""
     return t * (t >= 0)
 
-allFuncs = [unitstep_func,ramp_func]
+allFuncs = [unitstepFunc,rampFunc,rectFunc,triangFunc]
 
 plt.figure()
 for i, func in enumerate(allFuncs, start=1):
-    plt.subplot(2,1,i)
-    t = np.linspace(-2,2,1000)
+    plt.subplot(2,2,i)
+    t = np.linspace(-2,10,1000)
     plt.plot(t,func(t),'-b')
-    plt.ylim((-2,2))
+    plt.ylim((-2,10))
     plt.title(func.__doc__)
 plt.tight_layout()
 plt.show()
